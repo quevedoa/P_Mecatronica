590fd30e3ebce667c5ce953d8ffab85d64c188d2
quevedoa <queveduo@gmail.com>
2022-08-19 15:41:33 -0500

    Agregar archivos practica 2

--- Practica02/P2A1-Blink/P2A1-Blink.ino
+++ Practica02/P2A1-Blink/P2A1-Blink.ino
@@ -1,27 +1,12 @@
 #define LED 13
-#define BTN 12
-
-int btnState;
 
 void setup() {
-  pinMode(LED, OUTPUT)
-  pinMode(BTN, INPUT)
+  pinMode (LED, OUTPUT)
 }
 
 void loop() {
-  btnState = digitalRead(BTN);
-
-  if (btnState == 1) {
-    // Rutina de 4Hz
-    digitalWrite(LED, HIGH);
-    delay(250);
-    digitalWrite(LED, LOW);
-    delay(250);
-  } else {
-    // Rutina de 1Hz
-    digitalWrite(LED, HIGH);
-    delay(1000);
-    digitalWrite(LED, LOW);
-    delay(1000);
-  }
+  digitalWrite(LED,HIGH);
+  delay(1000);
+  digitalWrite(LED,LOW);
+  delay(1000);
 }
